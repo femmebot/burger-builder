b9a645b06b748351bbf3e0ad08a6174323003baa
femmebot <femmebot@gmail.com>
2018-08-23 00:05:32 -0400

    used WithClass hoc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 import Layout from './components/Layout/Layout';
+import WithClass from './hoc/WithClass';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <Layout/>
+      <WithClass styles={styles.App}>
+        <Layout>
         <p>Lorem ipsum dolor sit amet.</p>
-      </div>
+          </Layout>
+      </WithClass>
     );
   }
 }
