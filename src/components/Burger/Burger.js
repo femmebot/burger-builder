e12a8e517ef8c917274530999e3d30d45670819b
femmebot <femmebot@gmail.com>
2018-08-24 00:53:59 -0400

    +state to ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import styles from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+  // object of key value pairs converted into an array object
+  // value of object needed to determine # of each ingredient
+  const transformedIngredients = Object.keys( props.ingredients )
+    .map( igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredients key={igKey + i} type={igKey} />
+      });
+    });
   return (
     <div className={styles.Burger}>
       <BurgerIngredients type="bread-top"/>
-      <BurgerIngredients type="greens"/>
-      <BurgerIngredients type="cheese"/>
-      <BurgerIngredients type="meat"/>
+      {transformedIngredients}
       <BurgerIngredients type="bread-bottom"/>
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,22 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      greens: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    }
+  }
   render() {
     return (
       <Aux>
-          <Burger />
+          <Burger ingredients={this.state.ingredients}/>
         <div>
           Build controls
         </div>
